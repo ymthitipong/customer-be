3bb3a34f95beed27b54b9ce799fce677c38ab889
Thitipong <thitipong.pathom@gmail.com>
2026-04-17 15:18:56 +0700

    edit default port

--- ormconfig-migration.ts
+++ ormconfig-migration.ts
@@ -3,7 +3,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 const migrationOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5003'),
+  port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB || 'customer_db',
